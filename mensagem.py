b8a8251579ef7ef16e9ecb31ccd3a45d52d3a488
Sidney Max Azevedo <sidney.imob@hotmail.com>
2025-08-25 20:36:11 -0300

    Add files via upload
    Atividade da aula4 usando mensagens para desenvolvimento

---
+++ mensagem.py
@@ -0,0 +1,10 @@
+mensagem = int(input("Digite um número de 1 a 3 para um mensagem da sorte:"))
+
+if  mensagem == 1:
+    print("Olá, hoje seu dia será lindo!")
+elif mensagem == 2:
+    print("Olá, hoje o dia amanheceu mais vivo!")
+elif mensagem ==3:
+     print("Olá, talvez o seu dia não esteja bem, mas tudo irá melhorar!")
+else: 
+    print("Esse número não é da sorte!")
